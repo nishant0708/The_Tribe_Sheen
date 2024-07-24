e245a0b673d80a35438550e93924c0fbe265fd02
Nishant Kaushal <101548649+nishant0708@users.noreply.github.com>
2024-07-25 01:06:15 +0530

    fixed

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import "./About.css"
+import './About.css'
 const About = () => {
-  return (
-    <div>about</div>
-  )
+  return <div>about</div>
 }
 
 export default About
