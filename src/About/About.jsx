e10fa8a5b2765e87fbfda1707e71638ce5db7475
Prachi Mukati <prachi.mukati22@gmail.com>
2024-07-25 00:10:52 +0530

    about us

---
+++ src/About/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./About.css"
+const About = () => {
+  return (
+    <div>about</div>
+  )
+}
+
+export default About
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import About from '../About/About'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div> <About/></div>
+   
+  )
 }
 
 export default Home
