ecf719afc6d64798d7fddf6d0bfbd481e70bf733
Nishant Kaushal <101548649+nishant0708@users.noreply.github.com>
2024-07-25 18:54:10 +0530

    About_Us Added

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import About from '../About/About'
+import './Home.css'
 import Contact from '../Contact/Contact'
 
 const Home = () => {
   return (
-    <div> <About/>
-          <Contact/>
+    <div className="home">
+      <About />
+      <Contact />
     </div>
-   
   )
 }
 
