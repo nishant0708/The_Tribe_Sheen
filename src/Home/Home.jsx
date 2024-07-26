87fe7e948f37e4e438eef7633cba35b529e26294
RchtDshr <rachita17dashore@gmail.com>
2024-07-27 03:25:58 +0530

    added loader

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import About from '../About/About'
 import Contact from '../Contact/Contact'
+import Loader from '../Loader/Loader'
 
 const Home = () => {
   return (
     <div className="home">
+          <Loader/>
       <About />
       <Contact />
     </div>
