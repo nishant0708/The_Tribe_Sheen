a062ede9de9661f2816281b2f1b3fbbc40c52afd
Nishant Kaushal <101548649+nishant0708@users.noreply.github.com>
2024-07-27 01:56:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Home/Home'
+import Register from './Register/Register'
 
 
 const App = () => {
