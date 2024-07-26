9df3ea2f26a749a701fa205b9f87ab237a0d2134
kratik1093 <kratikmandloi03@gmail.com>
2024-07-27 01:14:01 +0530

    123

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Home/Home'
-import Register from './Register/Register'
+
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
+       
       </Routes>
     </Router>
   )
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import About from '../About/About'
 import Contact from '../Contact/Contact'
+import Sponsors from '../Sponsors/sponsors'
 
 const Home = () => {
   return (
     <div className="home">
       <About />
       <Contact />
+      <Sponsors/>
     </div>
   )
 }
