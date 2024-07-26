2393162fdba5e6ac129db4eae494084ae521f230
Nishant Kaushal <101548649+nishant0708@users.noreply.github.com>
2024-07-27 05:14:17 +0530

    Added perks

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import About from '../About/About'
 import Contact from '../Contact/Contact'
+import Perks from '../Perks/Perks'
 
 const Home = () => {
   return (
     <div className="home">
       <About />
+      <Perks />
       <Contact />
     </div>
   )
