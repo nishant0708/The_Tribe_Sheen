b25b32cc07653e056e327fced4b122647c1e396e
Nishant Kaushal <101548649+nishant0708@users.noreply.github.com>
2024-08-01 05:03:59 +0530

    fixed

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,47 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
-import "./Dashboard.css";  
-import image from "../assets/women.png";
-import logo from "../assets/Untitled design (1).png"; 
-
+import React, { useEffect, useState } from 'react'
+import { motion } from 'framer-motion'
+import './Dashboard.css'
+import image from '../assets/women.png'
+import logo from '../assets/Untitled design (1).png'
 
 export const Dashboard = () => {
-    const [contentHeight, setContentHeight] = useState(window.innerHeight * 0.6); // Default height
+  const [contentHeight, setContentHeight] = useState(window.innerHeight * 0.6) // Default height
 
   useEffect(() => {
     const handleResize = () => {
       // Update content height based on window width
       if (window.innerWidth < 480) {
-        setContentHeight(window.innerHeight * 0.7); // Example for very small screens
-      } else if (window.innerWidth < 738) {
-        setContentHeight(window.innerHeight * 0.85); // Example for small screens
-      }  else if (window.innerWidth < 788) {
-        setContentHeight(window.innerHeight * 0.9); // Example for small screens
-      } else if (window.innerWidth < 868) {
-        setContentHeight(window.innerWidth * 0.85); // Example for small screens
+        setContentHeight(window.innerHeight * 0.7) // Example for very small screens
       } else if (window.innerWidth < 1100) {
-        setContentHeight(window.innerHeight * 0.7); // Example for medium screens
-      }  else if (window.innerWidth < 1200) {
-        setContentHeight(window.innerHeight * 0.8); // Example for medium screens
+        setContentHeight(window.innerHeight * 0.7) // Example for medium screens
+      } else if (window.innerWidth < 1200) {
+        setContentHeight(window.innerHeight * 0.8) // Example for medium screens
       } else {
-        setContentHeight(window.innerHeight * 1); // Default for larger screens
+        setContentHeight(window.innerHeight * 1) // Default for larger screens
       }
-    };
+    }
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener('resize', handleResize)
 
     // Initial call to set height based on current window size
-    handleResize();
+    handleResize()
 
     // Cleanup the event listener on component unmount
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
 
   return (
     <div className="mainpage">
-      <div className="mainpage__content" style={{ height: `${contentHeight}px` }}>
-      <motion.div
+      <div className="overlay"></div>
+      <div
+        className="mainpage__content"
+        style={{ height: `${contentHeight}px` }}
+      >
+        <motion.div
           className="mainpage__topbar"
           initial={{ opacity: 0, y: -50 }}
           animate={{ opacity: 1, y: 0 }}
@@ -51,7 +47,11 @@ export const Dashboard = () => {
             <img src={logo} alt="logo" />
           </div>
           <div className="mainpage__topbar__subheader">
-            <p>Unleashing the Beauty Within: Tribe Sheen celebrates the Grace and Elegance of Miss and Mrs. contestants with Expert Grooming, Personality Development, and Fashion Flair.</p>
+            <p>
+              Unleashing the Beauty Within: Tribe Sheen celebrates the Grace and
+              Elegance of Miss and Mrs. contestants with Expert Grooming,
+              Personality Development, and Fashion Flair.
+            </p>
           </div>
           <div className="register__button">
             <p>Register</p>
@@ -60,14 +60,8 @@ export const Dashboard = () => {
         <div className="mainpage__coverpic"></div>
       </div>
       <div className="mainpage__pinkspacer">
-        <motion.img
-          src={image}
-          alt="coverpic2"
-          initial={{ opacity: 0, y: 50, x: "-50%" }}
-          animate={{ opacity: 1, y: 0, x: "-50%" }}
-          transition={{ duration: 1 }}
-        />
+        <img src={image} alt="coverpic2" />
       </div>
     </div>
-  );
+  )
 }
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,10 +2,6 @@ import React from 'react'
 import About from '../About/About'
 import Contact from '../Contact/Contact'
 import './Home.css'
-import Team from '../Our_Team/Team'
-import  { BreakpointProvider } from 'react-socks';
-import { DashboardComponent } from '../Dashboard/DashboardComponent';
-
 
 import Sponsors from '../Sponsors/sponsors'
 
@@ -14,24 +10,23 @@ import Perks from '../Perks/Perks'
 import Updates from '../Updates/Updates'
 import Teams from '../Teams/Teams'
 import Navbar from '../Navbar/Navbar'
+import { Dashboard } from '../Dashboard/Dashboard'
 
 const Home = () => {
   return (
     <div className="home">
       <Loader />
-        <BreakpointProvider>
-      <DashboardComponent />
-      </BreakpointProvider>
-      <About />
       <Navbar />
+      <Dashboard />
       <About />
+
       <Perks />
       <Sponsors />
       <Teams />
       <Updates />
       <Contact />
     </div>
-  )  
+  )
 }
 
 export default Home
